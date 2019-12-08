82b462e8f10a301cbdf66c0577be278251b8a68f
Nuno Cancelo <nuno.cancelo@gmail.com>
2019-12-08 21:04:55 +0000

    Blazor Playgroung initiation

---
+++ ServicesConsumed/Services/WeatherForecastService.cs
@@ -0,0 +1,28 @@
+﻿using ModelsAndAbstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesConsumed.Services
+{
+    public sealed class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
